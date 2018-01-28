d9b4f44e8d387d529f4e35c60342d40757771fcd
overqint <overqint@gmail.com>
2018-01-28 12:50:39 +0000

    test contract for validating a hardcoded public adress

---
+++ one.py
@@ -0,0 +1,22 @@
+from boa.blockchain.vm.Neo.Runtime import Notify, GetTrigger, CheckWitness
+from boa.blockchain.vm.Neo.Action import RegisterAction
+from boa.blockchain.vm.Neo.TriggerType import Application, Verification
+
+from boa.blockchain.vm.Neo.TransactionType import InvocationTransaction
+from boa.blockchain.vm.Neo.Transaction import *
+
+from boa.blockchain.vm.System.ExecutionEngine import GetScriptContainer, GetExecutingScriptHash
+from boa.blockchain.vm.Neo.TriggerType import Application, Verification
+from boa.blockchain.vm.Neo.Output import GetScriptHash, GetValue, GetAssetId
+from boa.blockchain.vm.Neo.Storage import GetContext, Get, Put, Delete
+
+
+def Main():
+    Owner = b'031a6c6fbbdf02ca351745fa86b9ba5a9452d785ac4f7fc2b7548ca2a46c4fcf4a'
+
+    result = CheckWitness(Owner)
+    if result:
+        print("Owner is caller")
+        return True
+    return False
+    
